ed23f4f92b03d34e7df7e6e214e9af3eb2912745
furimu <63914180+furimu@users.noreply.github.com>
2020-05-03 01:50:49 +0900

    Create profile.py

---
+++ cogs/profile.py
@@ -0,0 +1,26 @@
+from discord.ext import commands
+import discord
+
+class Send_Profile(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def profile(self, ctx, member: discord.Member = None):
+        if member is None:
+            member = ctx.author
+
+        boy = self.bot.get_channel(619930353267769344)
+        girl = self.bot.get_channel(619930418573213719)
+
+        async for message in boy.history(limit=None):
+            if message.author == member:
+                return await ctx.send(f"{str(message.author)}さんのプロフィールはこちらです。{mmessage.jump_url}")
+
+        else:
+            async def message in girl.history(limit = None):
+                if message.author == member:
+                    return await ctx.send(f"{str(message.author)}さんのプロフィールはこちらです。{mmessage.jump_url}")
+
+def setup(bot):
+    bot.add_cog(Send_Profile(bot)
