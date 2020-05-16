92c06a4b3eb9320ddbba19cdfc8d869d7fe1b979
furimu <63914180+furimu@users.noreply.github.com>
2020-05-17 02:49:10 +0900

    Update counter.py

--- cogs/counter.py
+++ cogs/counter.py
@@ -20,15 +20,20 @@ class Count(commands.Cog):
 
     @commands.command(name='ランダム')
     async def np(self, ctx, role:discord.Role):
+        vcd = {}
         vcm= []
         for member in role.members:
             if member.voice is not None and member.voice.channel.id == ctx.author.voice.channel.id:
                 vcm.append(member.id)
 
         random.shuffle(vcm)
+        cou= 1
+        for m in vcm:
+            vcd[str(cou)]= m
+            cou += 1
 
         e = discord.Embed(
-            description=',\n'.join(ctx.guild.get_member(m).mention for m in vcm))
+            description=',\n'.join(f"{k}: {ctx.guild.get_member(m).mention}" for k, m in vcd.items)
 
         await ctx.send(embed=e)
 
