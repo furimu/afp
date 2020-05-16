ecb6f5b49e605f4c3bef34f3dce9b8a6b78781b3
furimu <63914180+furimu@users.noreply.github.com>
2020-05-17 02:22:51 +0900

    Create counter.py

---
+++ cogs/counter.py
@@ -0,0 +1,9 @@
+from discord.ext import commands
+import discord
+class Count(commands.Cog):
+    def __init__(self, bot):
+        self.bot=bot
+
+
+    @commands.command(name='
+    async def np(self, ctx, role:discord.Role)
