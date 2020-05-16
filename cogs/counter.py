6b175d98a6cc61c308c2f45eca64d49d97e47494
furimu <63914180+furimu@users.noreply.github.com>
2020-05-17 02:27:16 +0900

    Update counter.py

--- cogs/counter.py
+++ cogs/counter.py
@@ -1,9 +1,16 @@
 from discord.ext import commands
 import discord
+import random
 class Count(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
 
 
-    @commands.command(name='
-    async def np(self, ctx, role:discord.Role)
+    @commands.command(name='ランダム')
+    async def np(self, ctx, role:discord.Role):
+        vcm= []
+        for member in role.members:
+            if member.voice is not None and member.voice.channel.id == ctx.author.voice.channel.id:
+                vcm.append(member.id)
+
+        random.shuffle(vcm)
