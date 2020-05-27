b1d9d4d7c560350ddedbb22067380e6324de64a2
furimu <63914180+furimu@users.noreply.github.com>
2020-05-27 21:42:12 +0900

    Update counter.py

--- cogs/counter.py
+++ cogs/counter.py
@@ -6,17 +6,7 @@ class Count(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        error = getattr(error, 'original', error)
-        
-        if isinstance(error, commands.CommandNotFound):
-            await ctx.send("そのコマンドは存在しないよ！")
- 
-        else:
-            msg= list(traceback.TracebackException.from_exception(error).format())
-            for i in range(0, len(msg), 1092):
-                await ctx.channel.send(f'```py\n{msg[i:i+1092]}\n```')
+   
 
     @commands.command(name='ランダム')
     async def np(self, ctx, role:discord.Role):
